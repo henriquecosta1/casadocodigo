61fe1c389c91653ad52fa98836824331f4f4de22
FABIO HENRIQUE CRISTO COSTA <02141087244@TRT8.net>
2022-02-11 18:07:54 -0300

    Disponibilizando dados no Postman no formado Json

--- src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
+++ src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.ejb.Stateful;
 import javax.persistence.Cache;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 
