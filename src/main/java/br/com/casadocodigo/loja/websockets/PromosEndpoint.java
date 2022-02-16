ac8240b9f6da25d7c218332427cdaf1cc06b7ba8
FABIO HENRIQUE CRISTO COSTA <02141087244@TRT8.net>
2022-02-16 10:03:27 -0300

    Atualizando projeto

--- src/main/java/br/com/casadocodigo/loja/websockets/PromosEndpoint.java
+++ src/main/java/br/com/casadocodigo/loja/websockets/PromosEndpoint.java
@@ -1,28 +1,44 @@
 package br.com.casadocodigo.loja.websockets;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.inject.Inject;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/canal/promos")
+import br.com.casadocodigo.loja.models.Promo;
+
+@ServerEndpoint(value="/canal/promos")
 public class PromosEndpoint {
 
+	@Inject
+	private UsuariosSession usuarios;
+	
 	@OnOpen
 	public void onMessage(Session session) {
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		usuarios.add(session);
+	}
+	
+	public void send(Promo promo) {
+		List<Session> sessions = usuarios.getUsuarios();
+		for (Session session : sessions) {
+			if(session.isOpen()) {
+				try {
+					session.getBasicRemote().sendText(promo.toJson());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}	
 		}
-		if(session.isOpen()) {
-			try {
-				session.getBasicRemote()
-					.sendText("{'titulo':'Livro JavaOO com 40% de desconto', 'livroId':35}");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+	}
+	
+	@OnClose
+	public void onClose(Session session, CloseReason closeReason) {
+		usuarios.remove(session);
+		System.out.println(closeReason.getCloseCode());
 	}
 }
